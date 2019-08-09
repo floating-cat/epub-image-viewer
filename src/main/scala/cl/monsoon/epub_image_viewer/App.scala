6f751b81d77a00d7f7884327b217600da857fd42
Lejia Chen <monsoon.cl@gmail.com>
2019-08-09 17:31:05 +0800

    Use Libarchivejs library to load epub file

--- src/main/scala/cl/monsoon/epub_image_viewer/App.scala
+++ src/main/scala/cl/monsoon/epub_image_viewer/App.scala
@@ -1,36 +1,33 @@
 package cl.monsoon.epub_image_viewer
 
+import cl.monsoon.epub_image_viewer.facade.{Archive, Options}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import org.scalajs.dom
 import slinky.core._
 import slinky.core.annotations.react
-import slinky.core.facade.ReactElement
 import slinky.web.html._
 
-import scala.scalajs.js
-import scala.scalajs.js.annotation.JSImport
-
-@JSImport("resources/App.css", JSImport.Default)
-@js.native
-object AppCSS extends js.Object
-
-@JSImport("resources/logo.svg", JSImport.Default)
-@js.native
-object ReactLogo extends js.Object
-
-@react class App extends StatelessComponent {
+@react object App {
   type Props = Unit
 
-  private val css = AppCSS
-
-  def render(): ReactElement =
+  val component: FunctionalComponent[Props] = FunctionalComponent[Props] { _ =>
     div(className := "App")(
-      header(className := "App-header")(
-        img(src := ReactLogo.asInstanceOf[String], className := "App-logo", alt := "logo"),
-        h1(className := "App-title")("Welcome to React (with Scala.js!)")
-      ),
-      p(className := "App-intro")(
-        "To get started, edit ",
-        code("App.scala"),
-        " and save to reload."
+      input(
+        `type` := "file",
+        onChange := (e => {
+          dom.console.log(e.target.files(0))
+          val archive = new Archive(e.target.files(0), new Options {
+            override def workerUrl: String = "worker-bundle.js"
+          })
+
+          archive
+            .open()
+            .toFuture
+            .flatMap(_.extractFiles().toFuture)
+            .foreach(o => dom.console.log(o))
+        })
       )
     )
+  }
 }
