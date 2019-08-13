5db682c3d78b54c7b9989a2fb59d393b5e16ade1
floating-cat <monsoon.cl@gmail.com>
2019-08-13 16:59:35 +0800

    Use sbt-tpolecat for better code style

--- src/main/scala/cl/monsoon/epub_image_viewer/facade/Archive.scala
+++ src/main/scala/cl/monsoon/epub_image_viewer/facade/Archive.scala
@@ -2,12 +2,13 @@ package cl.monsoon.epub_image_viewer.facade
 
 import org.scalajs.dom.File
 
+import scala.annotation.unused
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
 @JSImport("libarchive.js/main.js", "Archive")
 @js.native
-class Archive(file: File, options: Options) extends js.Object {
+class Archive(@unused file: File, @unused options: Options) extends js.Object {
   def open(): js.Promise[Archive] = js.native
 
   def extractFiles(): js.Promise[js.Dictionary[File]] = js.native
