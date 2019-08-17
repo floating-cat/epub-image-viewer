34dcd82cdccb9fc91e33bc043c5c2a862716c5d9
floating-cat <monsoon.cl@gmail.com>
2019-08-17 17:36:11 +0800

    Add readme and fix a typo for the project name

--- webpack/scalajs-entry.js
+++ webpack/scalajs-entry.js
@@ -1,13 +1,13 @@
 if (process.env.NODE_ENV === "production") {
-    const opt = require("./epub-image-viewer-opt.js");
+    const opt = require("./epub-images-viewer-opt.js");
     opt.main();
     module.exports = opt;
 } else {
     var exports = window;
-    exports.require = require("./epub-image-viewer-fastopt-entrypoint.js").require;
+    exports.require = require("./epub-images-viewer-fastopt-entrypoint.js").require;
     window.global = window;
 
-    const fastOpt = require("./epub-image-viewer-fastopt.js");
+    const fastOpt = require("./epub-images-viewer-fastopt.js");
     fastOpt.main()
     module.exports = fastOpt;
 
