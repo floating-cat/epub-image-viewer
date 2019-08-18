58aecf91bcdd11260578e9dbd8cf28e48cf4ad24
floating-cat <monsoon.cl@gmail.com>
2019-08-18 19:50:53 +0800

    Support directories selection

---
+++ src/main/scala/cl/monsoon/epub_image_viewer/util/CustomAttributeUtil.scala
@@ -0,0 +1,7 @@
+package cl.monsoon.epub_image_viewer.util
+
+import slinky.core.CustomAttribute
+
+object CustomAttributeUtil {
+  val webkitdirectory = new CustomAttribute[String]("webkitdirectory")
+}
