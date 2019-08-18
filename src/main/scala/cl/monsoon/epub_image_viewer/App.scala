3a414081112538f3e46e8bf3a7e575d22198ecf5
floating-cat <monsoon.cl@gmail.com>
2019-08-18 19:50:53 +0800

    Support directories selection

--- src/main/scala/cl/monsoon/epub_image_viewer/App.scala
+++ src/main/scala/cl/monsoon/epub_image_viewer/App.scala
@@ -1,94 +1,31 @@
 package cl.monsoon.epub_image_viewer
 
-import cats.data.NonEmptyChain
 import cats.implicits._
-import cl.monsoon.epub_image_viewer.EpubReader.{Errors, ImageFileDataUrl}
-import cl.monsoon.epub_image_viewer.facade.Archive
-import org.scalajs.dom.console.log
-import org.scalajs.dom.raw.KeyboardEvent
-import org.scalajs.dom.{document, window}
+import cl.monsoon.epub_image_viewer.EpubReader.ImageFileDataUrl
 import slinky.core._
 import slinky.core.annotations.react
 import slinky.core.facade.Hooks._
 import slinky.web.html._
-import zio.{DefaultRuntime, UIO, ZIO}
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
-import scala.util.chaining._
 
-@JSImport("resources/App.css", JSImport.Default)
+@JSImport("resources/Main.css", JSImport.Default)
 @js.native
-object AppCSS extends js.Object
+object MainCSS extends js.Object
 
 @react object App {
   type Props = Unit
 
-  val css: AppCSS.type = AppCSS
-
   val component: FunctionalComponent[Props] = FunctionalComponent[Props] { _ =>
     val (imageFileDataUrls, imageFileDataUrlsUpdateState) = useState(none[Seq[ImageFileDataUrl]])
-    val (imageViewedIndex, imageViewedIndexUpdateState) = useState(0)
-    val (autoFitImage, autoFitImageUpdateState) = useState(true)
-
-    useEffect { () =>
-      // Use js function explicit because of  https://stackoverflow.com/q/57148965/2331527
-      val keyDownListener: js.Function1[KeyboardEvent, Unit] = { e: KeyboardEvent =>
-        imageFileDataUrls.foreach { urls =>
-          e.key match {
-            case "ArrowLeft" =>
-              if (urls.lengthIs >= imageViewedIndex + 1 + 1) {
-                imageViewedIndexUpdateState(imageViewedIndex + 1)
-              }
-            case "ArrowRight" =>
-              if (imageViewedIndex - 1 >= 0) {
-                imageViewedIndexUpdateState(imageViewedIndex - 1)
-              }
-            case "f" => autoFitImageUpdateState(!_)
-            case "e" =>
-              imageFileDataUrlsUpdateState(none)
-              imageViewedIndexUpdateState(0)
-            case _ =>
-          }
-        }
-      }
-
-      val eventName = "keydown"
-      document.addEventListener(eventName, keyDownListener)
-      () => document.removeEventListener(eventName, keyDownListener)
-    }
 
     val mainContent = if (imageFileDataUrls.isEmpty || imageFileDataUrls.get.isEmpty) {
-      input(
-        `type` := "file",
-        multiple,
-        onChange := { e =>
-          val epubFile = e.target.files(0)
-          ZIO
-            .effectTotal(log(epubFile))
-            .flatMap(_ => ZIO.fromFuture(ec => Archive.extractZip(epubFile)(ec)))
-            .mapError[Errors](
-              e => NonEmptyChain(s"Can't load ${epubFile.name}: " + e.getMessage)
-            )
-            .flatMap(new EpubReaderJs().getImageDataUrl.provide)
-            .flatMap(
-              imageDataUrls =>
-                UIO.effectTotal {
-                  imageFileDataUrlsUpdateState(Some(imageDataUrls))
-                }
-            )
-            .flatMapError(o => {
-              window.alert(o.mkString_("", "\n", ""))
-              ZIO.none
-            })
-            .pipe(new DefaultRuntime {}.unsafeRunAsync_(_))
-        }
+      FilesOrDirectoriesSelectView(
+        (imageFileDataUrlsUpdateState(_: Option[Seq[ImageFileDataUrl]])).compose(Some(_))
       )
     } else {
-      img(
-        className := "illustration" + (if (autoFitImage) " auto_fit" else ""),
-        src := imageFileDataUrls.get(imageViewedIndex)
-      )
+      IllustrationDisplayView((imageFileDataUrls.get, imageFileDataUrlsUpdateState))
     }
 
     div(className := "App")(mainContent)
---
+++ src/test/scala/cl/monsoon/epub_image_viewer/util/SortUtilTest.scala
@@ -0,0 +1,76 @@
+package cl.monsoon.epub_image_viewer.util
+
+import cats.implicits._
+import verify.BasicTestSuite
+
+object SortUtilTest extends BasicTestSuite {
+  test(
+    "the book names follow the convention declared in the sort method " +
+      "can be sorted by it's book volume number"
+  ) {
+    val bookNames = Seq(
+      "Absalom, Absalom! 1",
+      "Absalom, Absalom! 10",
+      "Absalom, Absalom! 03",
+      "Absalom, Absalom! 2"
+    )
+    val sortedBookNames = Seq(
+      "Absalom, Absalom! 1",
+      "Absalom, Absalom! 2",
+      "Absalom, Absalom! 03",
+      "Absalom, Absalom! 10"
+    )
+
+    assert(SortUtil.sortForASeriesThings(bookNames) == sortedBookNames)
+  }
+
+  test(
+    "the book names don't follow the convention declared in the sort method" +
+      "can be sorted in lexicographic order"
+  ) {
+    val bookNames = Seq(
+      "4 Absalom, Absalom!",
+      "1 Absalom, Absalom!",
+      "2 Dance Dance Dance",
+      "0 Dance Dance Dance"
+    )
+    val sortedBookNames = Seq(
+      "0 Dance Dance Dance",
+      "1 Absalom, Absalom!",
+      "2 Dance Dance Dance",
+      "4 Absalom, Absalom!"
+    )
+
+    assert(SortUtil.sortForASeriesThings(bookNames) == sortedBookNames)
+  }
+
+  test(
+    "the book names follow the convention declared in this sort method " +
+      "can be sorted by it's book volume number" +
+      "and the book names don't follow the convention declared in this sort method" +
+      "can be sorted in lexicographic order and all placed after the former"
+  ) {
+    val bookNames = Seq(
+      "4 Absalom, Absalom!",
+      "1 Absalom, Absalom!",
+      "Absalom, Absalom! 1",
+      "Absalom, Absalom! 2",
+      "2 Dance Dance Dance",
+      "0 Dance Dance Dance",
+      "Absalom, Absalom! 10",
+      "Absalom, Absalom! 03"
+    )
+    val sortedBookNames = Seq(
+      "Absalom, Absalom! 1",
+      "Absalom, Absalom! 2",
+      "Absalom, Absalom! 03",
+      "Absalom, Absalom! 10",
+      "0 Dance Dance Dance",
+      "1 Absalom, Absalom!",
+      "2 Dance Dance Dance",
+      "4 Absalom, Absalom!"
+    )
+
+    assert(SortUtil.sortForASeriesThings(bookNames) == sortedBookNames)
+  }
+}
